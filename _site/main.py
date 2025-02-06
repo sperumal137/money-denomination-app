71eea5b6d59caba5b3b7f46e1460eb7a195390e3
SPK <sathiyaraj.perumal@gmail.com>
2025-02-06 13:59:47 +0530

    Updated Flask app with new changes

--- _site/main.py
+++ _site/main.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/data')
+def get_data():
+    
+    return jsonify({'message': 'Python is running!'})
+
 
 def calculate_denominations(notes):
     denominations = [500, 200, 100, 50, 20, 10]
